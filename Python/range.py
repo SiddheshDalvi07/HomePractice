306f82ae720e7c38bba149c4e366b2f6a607e76e
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-02-27 17:11:14 +0530

    python lec

--- Python/range.py
+++ Python/range.py
@@ -1,6 +1,10 @@
-for x in range(10):
-    print(x)
-for x in range(1,10):
-    print(x)
-for x in range(0,10,2):
-    print(x)
+# for x in range(10):
+#     print(x)
+# for x in range(1,10):
+#     print(x)
+# for x in range(0,10,2):
+#     print(x)
+
+z = range(1,11,1)
+l = list(z)
+print(l)
