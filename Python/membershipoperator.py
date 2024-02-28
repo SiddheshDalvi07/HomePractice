8ad5b8ccfeead319fa35df998cdc16c8ef00f438
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-02-28 17:12:34 +0530

    python lec

---
+++ Python/membershipoperator.py
@@ -0,0 +1,6 @@
+string="IT vedant"
+print('IT' in string)
+print('ti' in string)
+print()
+print('IT' not in string)
+print('Ti' not in string)
--- Python/stringFunction.py
+++ Python/stringFunction.py
@@ -24,24 +24,23 @@ print(s.replace('o', '*')) #replace  all o with *
 print(s.split()) #convert to a list
 print(s[2])  #indexing
 print(s[2:8]) #slicing string_name[start:end:step] step default value is 1
-
-
-
-
-
-
-
+print("swapcase : ",s.swapcase())  #swaps case of alphabets
+print("title : ",s.title())     #capitalize first letter of each word
+print("strip : ",s.strip()) 
 #ss=multi.split()  # Return a list of the substrings in the string, using sep as the separator string.
-
-
-
-
-
-
-
-
-
-
 # print(ss)
 # print(ss.pop(2))
 # print(ss)
+
+s="Itvedant"
+print("Index -  Character")
+for i in range(0,len(s),1):
+      print(i,"      -     ",s[i])
+
+s1="Itvedant"
+print("Character")
+for x in s1:    
+      print(x)
+
+
+
