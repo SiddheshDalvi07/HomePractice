3b72a986c99e76f41d47a96ad52e8502cc2c4707
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-03-11 21:03:19 +0530

    py lec

--- Python/dictionary.py
+++ Python/dictionary.py
@@ -13,6 +13,14 @@ print(d["name"])
 d["email"]= "dennis@gmail.com"
 print(d)
 
+#update values in dict
+d["age"] = 45
+print(d)
+
+#remove value from dictionary using del keyword
+del d["city"]
+print(d)
+
 product1={
     "Name":"Laptop",
     "Price":5000,
@@ -38,4 +46,14 @@ for i,j in product1.items():
     print(i,":",j)
 
 
+d1 = {"s1":"v1","s2":"v2"}
+d2 = {"name": "Dennis", "age":36 }
+
+#copy one dict to another
+d3 = {**d1, **d2}
+print(d3)
+
+#merge two dict to another with constructor
+d3=dict(d1,**d2)
+print(d3)
 
