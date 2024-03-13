a9749cea790ddec9898b2f47ee32a1dbf35c3ee3
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-03-13 23:15:51 +0530

    py assg

--- Python/average.py
+++ Python/average.py
@@ -10,3 +10,6 @@ print(f"The average of the three strings is {int(average(a,b,c))} ")
 """f-strings (formatted string literals) allow you to embed expressions directly inside string literals, 
 making it easy to format strings with variables or expressions. 
 The expressions inside curly braces {} are evaluated and replaced with their values."""
+
+
+
