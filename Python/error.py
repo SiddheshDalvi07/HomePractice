998a8dbeb23d49bc6ae48e471688045e7a8d00bc
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-03-27 21:48:18 +0530

    py lec

--- Python/error.py
+++ Python/error.py
@@ -35,16 +35,35 @@
 # finally:
 #     print("resource closed")
 
-try: 
-    a = int(input("enter the number:"))
-    b = int(input("enter the number:"))
-    print(a/b)
-except ZeroDivisionError as e:  
-    print("you can't divide any number by zero")
-    # print(e)
-except ValueError as e:
-    print("You should enter the integer value only")
-except Exception as e:
-    print("Something went wrong")
-finally:
-    print("resource closed")
+# try: 
+#     a = int(input("enter the number:"))
+#     b = int(input("enter the number:"))
+#     print(a/b)
+# except ZeroDivisionError as e:  
+#     print("you can't divide any number by zero")
+#     # print(e)
+# except ValueError as e:
+#     print("You should enter the integer value only")
+# except Exception as e:
+#     print("Something went wrong")
+# finally:
+#     print("resource closed")
+
+
+class Division:
+    def  __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        
+    def division(self):
+        try:
+            result = self.num1 / self.num2
+            print(result)
+        except ZeroDivisionError as e:  
+            print("you can't divide any number by zero")
+            
+d1 = Division(5,0) 
+d1.division()
+
+d2 = Division(15,10) 
+d2.division()
