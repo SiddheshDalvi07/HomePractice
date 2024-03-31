7d6d0a5a13ad2bfb8ef72d436d149af502d9cc8a
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-03-31 20:57:04 +0530

    py lec

--- Python/file3.py
+++ Python/file3.py
@@ -1,8 +1,30 @@
+# file = open("F:/coding practice/HomePractice/Python/example.txt","r")
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readlines())
+# file.close()
+
 file = open("F:/coding practice/HomePractice/Python/example.txt","r")
-print(file.readline())
-print(file.readline())
-print(file.readline())
-print(file.readline())
-print(file.readlines())
+print("current position :",file.tell())
+
+#reposition pointer
+print(file.seek(0,0))  #set to the beginning of the file (whence=0)
+# print("after seek: current position :",file.tell())
+
+print(file.seek(2,0))    #set to current poistion of the file (whence=1)
+print(file.read(10))
 file.close()
+
+import os
+fname="example2.txt"
+print(fname)
+print(os.path.abspath(fname))   #get absolute path of a filename
+
+baseDir = os.path.dirname(os.path.abspath(__file__))     #get base directory of python script
+print(baseDir)
+
+templateDir = os.path.join(baseDir,"templates") 
+print(templateDir)
 
