c7cef23e56f233a96101b86a30f07fd94b01d4f1
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-03-31 13:01:09 +0530

    py lec

---
+++ Python/file3.py
@@ -0,0 +1,8 @@
+file = open("F:/coding practice/HomePractice/Python/example.txt","r")
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readlines())
+file.close()
+
