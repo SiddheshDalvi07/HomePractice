388b4bd71b5aef2e75da7c5fecd4a230a266a5cc
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-04-03 21:10:33 +0530

    django test

--- Django/test1/app1/views.py
+++ Django/test1/app1/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 
 def  index(request):
     return HttpResponse('hello world')
+
+def about(request):
+    return HttpResponse("about page")
+
+def contact(request):
+    return render(request,"index.html")
