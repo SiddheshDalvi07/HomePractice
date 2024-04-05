3fa45d60b68a31015877d1a8c1c3bae5bd24bc9d
Siddheshdalvi07 <dalvisiddhesh3@gmail.com>
2024-04-05 17:46:01 +0530

    django test

--- Django/test1/app1/urls.py
+++ Django/test1/app1/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.index),
     path("contact/about",views.about),
-    path("contact/",views.contact)
+    path("about/",views.about),
+    path("contact/",views.contact),
 ]
