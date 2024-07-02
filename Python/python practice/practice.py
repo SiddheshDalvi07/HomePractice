7d1d50da1aa4108d3ea5d125bc0cd37937d977d1
SiddheshDalvi07 <dalvisiddhesh3@gmail.com>
2024-07-02 16:07:17 +0530

    py lgb

--- Python/python practice/practice.py
+++ Python/python practice/practice.py
@@ -85,5 +85,22 @@
 # str = input("enter the string: ")
 # print(palindrome(str))
 
+# def foobar(n):
+#     for i in range(1,n+1):
+#         if i%3==0 and i%5==0:
+#             print ("foobar")
+#         elif i%3==0:
+#             print ("foo")
+#         elif i%5==0:
+#             print ("bar")
+#         else:
+#             print (i)
+
+# n = int(input("Enter the number: "))
+# foobar(n)
+
+
+def prime(n):
+    
 
 
